425dd9f8c4d23fd84805a13e9bf8e1439238cdfd
romaklayt <romakolyago@hotmail.com>
2018-11-06 23:34:24 +0300

    add gpsapi

--- SortPhoto/SortByCity.cs
+++ SortPhoto/SortByCity.cs
@@ -1,8 +1,6 @@
 ﻿using System.IO;
-using System.Net;
-using System.Text;
-using System.Xml;
 using ExtractMetadataAndParse;
+using GpsCore;
 
 namespace SortPhoto
 {
@@ -18,7 +16,6 @@ namespace SortPhoto
             _path = userPath;
             _files = Directory.GetFiles(_path);
             _subpath = new DirectoryInfo(_path).Name + "-Kolyago";
-            var urlApi = "https://geocode-maps.yandex.ru/1.x/?geocode=";
             var counter = 0;
             foreach (var file in _files)
             {
@@ -26,33 +23,13 @@ namespace SortPhoto
                 var (gpsW, gpsH, gpsCoordinateNotFound) = ParseGps.Parse(file);
 
                 if (gpsCoordinateNotFound) continue;
-
-                var xd = new XmlDocument();
-                using (var client = new WebClient())
-                {
-                    client.Encoding = Encoding.UTF8;
-                    client.Headers.Add("Accept-Language", " en-US");
-                    client.Headers.Add("Accept", "application/xml");
-                    client.Headers.Add("User-Agent", "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)");
-                    xd.LoadXml(client.DownloadString(urlApi + gpsH + "," + gpsW));
-                }
-
-                var geoObjectTempCountry = xd.GetElementsByTagName("Country");
-
-                foreach (XmlNode childCountry in geoObjectTempCountry[0].ChildNodes)
-                    if (childCountry.Name == "AddressLine")
-                    {
-                        var txtXmlAddress = childCountry.FirstChild.InnerText;
-                        txtXmlAddress = txtXmlAddress.Remove(txtXmlAddress.IndexOf(','),
-                            txtXmlAddress.Length - txtXmlAddress.IndexOf(','));
-
-                        var destFileName = new DirectoryInfo(_path + "\\" + _subpath + "\\" + txtXmlAddress);
-                        if (!destFileName.Exists) destFileName.Create();
-                        var extension = Path.GetExtension(file);
-                        var fileNameOnly = Path.GetFileNameWithoutExtension(file);
-                        File.Copy(file, destFileName + "\\" + fileNameOnly + extension, true);
-                        break;
-                    }
+                var city = GpsApi.FindCity(gpsW, gpsH);
+                if (city == null) continue;
+                var destFileName = new DirectoryInfo(_path + "\\" + _subpath + "\\" + city);
+                if (!destFileName.Exists) destFileName.Create();
+                var extension = Path.GetExtension(file);
+                var fileNameOnly = Path.GetFileNameWithoutExtension(file);
+                File.Copy(file, destFileName + "\\" + fileNameOnly + extension, true);
             }
 
             ProgressBar.DrawTextProgressBar(_files.Length, _files.Length);
